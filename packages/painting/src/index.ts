f7a6bcf203237081b74f9f1be0659f6d3c8f72f0
Z.X <x@z-x.vip>
2026-05-16 14:16:17 +0800

    feat(painting): add stroke manipulation utilities (add, remove, update, pick, clear)

--- packages/painting/src/index.ts
+++ packages/painting/src/index.ts
@@ -7,3 +7,10 @@ export type {
 } from "./components/DrawingSurface";
 export { DrawingSurface } from "./components/DrawingSurface";
 export type { DrawingStrokeSmoothingOptions } from "./stroke-helpers";
+export {
+  addStroke,
+  clearStrokes,
+  pick,
+  removeStroke,
+  updateStroke,
+} from "./utils";
